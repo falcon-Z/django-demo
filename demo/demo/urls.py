1b7d4ae19d5e04175d53b2f6fd06291bf59bdb02
Madhan <madhan_sai@yahoo.in>
2020-10-03 12:54:35 +0530

    fixes

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +25,5 @@ urlpatterns = [
     path('todo/', include('todo.urls'), name='todo'),
     path('portfolio/', include('portfolio.urls'), name='portfolio'),
     path('blog/', include('blog.urls'), name='blog'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
